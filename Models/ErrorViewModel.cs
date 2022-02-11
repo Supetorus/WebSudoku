e3f93231670d954a0f521757d0b322d6e2bf4f08
Supetorus <66396555+Supetorus@users.noreply.github.com>
2022-02-10 19:12:04 -0700

    Add VS ASP.NET Core MVC Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebSudoku.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
